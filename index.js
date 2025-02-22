c1667ce393eed20928c79c13fe7e5ce529a5b26d
Md Siyam <mdsiyam9424@gmail.com>
2025-02-22 19:09:56 +0600

    update status on dropping

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const port = process.env.PORT || 5000;
 
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:5173','https://ph-task-management.netlify.app',], 
+}))
 
 app.get("/", (req, res)=>{
     res.send("Task management server")
@@ -15,7 +17,7 @@ app.get("/", (req, res)=>{
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER}:${process.env.USER_KEY}@cluster0.ttcu5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,7 +32,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const taskCollection = client.db("Task-management").collection("tasks")
     const userCollection = client.db("Task-management").collection("users")
@@ -43,7 +45,11 @@ async function run() {
 
     app.post("/users", async(req, res)=>{
       const user = req.body;
-      
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({ message: "User already exist", insertedId: null })
+      }
       const result = await userCollection.insertOne(user)
       res.send(result)
     })
@@ -62,9 +68,30 @@ async function run() {
         res.send(result)
     })
 
+    app.put("/tasks/:id", async(req, res)=>{
+      const {status} = req.body;
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)} 
+      const updatedDoc = {
+        $set:{
+          status: status
+        }
+      }
+      const reslut = await taskCollection.updateOne(filter,updatedDoc)
+      res.send(reslut)
+      // console.log("from 71",status,id);
+    })
+
+    app.delete("/tasks/:id", async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await taskCollection.deleteOne(query);
+      res.send(result)
+    })
+
     app.post("/tasks", async(req, res)=>{
         const task = req.body;
-        console.log(task);
+        
         const result = await taskCollection.insertOne(task)
         res.send(result)
     })
@@ -72,8 +99,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
